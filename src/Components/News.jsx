d1470ae0d2a97d4787a0d4b940e4fd9f780b6be4
Evans Wanjau <evanswanjau@gmail.com>
2023-03-07 13:51:12 +0300

    Updated news UI

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,14 +1,18 @@
+import { Slide } from "react-reveal";
+
 export const News = ({ data: { title, published, excerpt, image, url } }) => {
     return (
-        <a href={`/media-centre/news/${url}`} className="group">
-            <div
-                className={`bg-[url('${image}')] bg-cover bg-center bg-no-repeat rounded-lg text-white h-40`}
-            ></div>
-            <p className="py-2 text-gray-400">{published}</p>
-            <h3 className="text-xl text-black group-hover:text-[#329E49] transition duration-300 ease-in-out">
-                {title}
-            </h3>
-            <p>{excerpt}</p>
-        </a>
+        <Slide bottom carousel>
+            <a href={`/media-centre/news/${url}`} className="group">
+                <div
+                    className={`bg-[url('${image}')] bg-cover bg-center bg-no-repeat rounded-lg text-white h-40`}
+                ></div>
+                <p className="text-sm py-2 text-gray-400">{published}</p>
+                <h3 className="text-black group-hover:text-[#ED7423] font-medium transition duration-300 ease-in-out">
+                    {title}
+                </h3>
+                <p>{excerpt}</p>
+            </a>
+        </Slide>
     );
 };
