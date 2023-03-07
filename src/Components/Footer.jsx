9a842935150d147558ed50c74c6765569d36bb8f
Evans Wanjau <evanswanjau@gmail.com>
2023-03-07 13:19:01 +0300

    Added Footer Components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,171 +1,136 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { BsTelephone } from 'react-icons/bs';
-import { AiOutlineMail } from 'react-icons/ai';
-import logo from '../assets/Logo.svg';
+import { Link } from "react-router-dom";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
+
 function Footer() {
-	const [Elements, SetElement] = useState([
-		{
-			Link: '/about',
-			name: 'WHO WE ARE',
-			elements: [
-				{
-					link: '/about',
-					name: 'About Us',
-				},
-				{
-					link: '/about',
-					name: 'Our Mission',
-				},
-				{
-					link: '/about',
-					name: 'Our Vision',
-				},
-				{
-					link: '/about',
-					name: 'Our Parteners',
-				},
-				{
-					link: '/team',
-					name: 'Our Team',
-				},
-			],
-		},
-		{
-			Link: '/donate',
-			name: 'GET INVOLVED',
-			elements: [
-				{
-					link: '/donate',
-					name: 'Careers',
-				},
-				{
-					link: '/donate',
-					name: 'Consultancy',
-				},
-				{
-					link: '/careers',
-					name: 'Donate',
-				},
-				{
-					link: '/careers',
-					name: 'Funding',
-				},
-				{
-					link: '/contact',
-					name: 'Contact Us',
-				},
-			],
-		},
-		{
-			Link: '/donate',
-			name: 'MEADIA CENTRE',
-			elements: [
-				{
-					link: '/press',
-					name: 'News',
-				},
-				{
-					link: '/eventread',
-					name: 'Events',
-				},
-				{
-					link: '/about',
-					name: 'Projects',
-				},
-				{
-					link: '/press',
-					name: 'Press Release',
-				},
-				{
-					link: '/about',
-					name: 'Gallery',
-				},
-			],
-		},
-	]);
-	return (
-		<footer className=" w-full   h-fit text-white p-12  bg-[#0B0706] ">
-			<div className=" w-full border-b pb-6 border-[#71777D] justify-between  flex flex-col lg:flex-row  items-center lg:items-start lg:space-x-12  ">
-				<div className="space-y-5 lg:w-1/5 ">
-					<div className="logo">
-						<img
-							src={logo}
-							alt=""
-						/>
-					</div>
-					<div className="text-[#bebebe]">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						</p>
-					</div>
-					<div className="lg:space-y-2 flex lg:flex-col justify-between ">
-						<div className="email flex items-center space-x-2">
-							<AiOutlineMail></AiOutlineMail>
-							<Link to="mailto:agensi@mail.com">agensi@mail.com</Link>
-						</div>
-						<div className="phone flex items-center space-x-2">
-							<BsTelephone></BsTelephone>
-							<Link to="tel:12 3456 7890">+ 12 3456 7890</Link>
-						</div>
-					</div>
-				</div>
-				<div className="grid lg:mt-0 mt-10  grid-cols-3  lg:space-x-5 lg:space-y-0 gap-3  lg:w-2/5 w-full  ">
-					{Elements.map((ele) => (
-						<div className=" space-y-5">
-							<Link to={ele.Link}>{ele.name}</Link>
-							<ul className="space-y-2 text-[#bebebe]">
-								{ele.elements.map((subele) => (
-									<li>
-										<Link
-											to={subele.link}
-											title={subele.name}
-										>
-											{subele.name}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</div>
-					))}
-				</div>
-				<div className="space-y-5 lg:w-1/5 lg:mt-0 mt-5">
-					<div className="logo">
-						<p>NEWSLETTER</p>
-					</div>
-					<div className="text-[#bebebe]">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco
-						</p>
-					</div>
-					<div>
-						<form
-							onSubmit={(e) => e.preventDefault()}
-							className="email w-full flex justify-start space-x-0  items-center "
-						>
-							<input
-								className="bg-transparent border border-gray-400 p-1 w-4/6 rounded"
-								type="email"
-								placeholder="Your email"
-							/>
-							<button
-								className="bg-white p-1 w-2/6 text-black "
-								onClick={() => console.log('Add To Email List')}
-							>
-								SUBSCRIBE
-							</button>
-						</form>
-					</div>
-				</div>
-			</div>
-			<div className="pt-5 pb-20 text-[#bebebe]">
-				<div className="rights">© Finsweet Ltd 2023 | All rights reserved</div>
-			</div>
-		</footer>
-	);
+    const links = [
+        {
+            name: "WHO WE ARE",
+            links: [
+                { name: "About Us", url: "/who-we-are/about-us" },
+                {
+                    name: "Our Mission",
+                    url: "/who-we-are/about-us#our-mission",
+                },
+                { name: "Our Vision", url: "/who-we-are/about-us#our-vision" },
+                {
+                    name: "Our Partners",
+                    url: "/who-we-are/about-us#our-partners",
+                },
+                { name: "Our Team", url: "/who-we-are/our-team" },
+            ],
+        },
+        {
+            name: "GET INVOLVED",
+            links: [
+                { name: "Careers", url: "/get-involved/careers" },
+                { name: "Consultancy", url: "/get-involved/consultancy" },
+                { name: "Donate", url: "/get-involved/donate" },
+                { name: "Funding", url: "/get-involved/funding-opportunities" },
+                { name: "Contact Us", url: "/get-involved/contact-us" },
+            ],
+        },
+        {
+            name: "MEDIA CENTRE",
+            links: [
+                { name: "News", url: "/media-centre/news" },
+                { name: "Events", url: "/media-centre/events" },
+                { name: "Projects", url: "/media-centre/projects" },
+                { name: "Press", url: "/media-centre/press-release" },
+                { name: "Gallery", url: "/media-centre/photo-gallery" },
+            ],
+        },
+    ];
+
+    return (
+        <footer className="px-10 pt-5 text-gray-600">
+            <div className="flex flex-row space-x-8">
+                <div className="w-3/12">
+                    <img
+                        className="w-40 mb-5"
+                        src="/ccak_side_logo.png"
+                        alt="CCAK Logo"
+                    />
+                    <p className="py-3">
+                        The Clean Cooking Association of Kenya is a sector
+                        association with a vision to see universal access and
+                        adoption of clean cooking solutions and practices in
+                        kenya, through a sustainable market environment.
+                    </p>
+                    <div className="flex flex-row space-x-4 hover:text-[#ED7423] my-2">
+                        <EnvelopeIcon className="w-5 h-5" />
+                        <Link to="mailto:info@ccak.or.ke">info@ccak.or.ke</Link>
+                    </div>
+                    <div className="flex flex-row space-x-4 hover:text-[#ED7423] my-3">
+                        <PhoneIcon className="w-5 h-5" />
+                        <Link to="tel:+254707111669">0707 111 669</Link>
+                    </div>
+                </div>
+                <div className="w-6/12 pt-5">
+                    <div className="flex row justify-around">
+                        {links.map((link) => {
+                            return (
+                                <div>
+                                    <h3 className="text-black font-medium my-5">
+                                        {link.name}
+                                    </h3>
+                                    <ul>
+                                        {link.links.map((link) => {
+                                            return (
+                                                <a href={link.url}>
+                                                    <li className="text-gray-600 hover:text-[#ED7423] my-6">
+                                                        {" "}
+                                                        {link.name}
+                                                    </li>{" "}
+                                                </a>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="w-3/12 pt-5 font-manjari">
+                    <h3 className="text-black font-medium my-5">NEWSLETTER</h3>
+                    <p className="py-3">
+                        Stay up-to-date with the latest news and updates on
+                        clean cooking in Kenya by signing up for our newsletter!
+                        As a subscriber, you'll be the first to hear about our
+                        latest projects, events, and advocacy efforts.
+                    </p>
+                    <input
+                        className="w-full border border-1 outline-0 rounded-md pt-3 pb-2 px-3 focus:border-gray-400 focus:border-1"
+                        type="email"
+                        placeholder="Your email"
+                    />
+                    <button className="my-3 bg-[#329E49] leading-none pb-2 pt-3 px-2 font-manjari text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest">
+                        SUBSCRIBE
+                    </button>
+                </div>
+            </div>
+            <div className="flex flex-row border-t py-5 mt-12 text-sm">
+                <p className="w-6/12">
+                    © Clean Cooking Association Kenya 2023 &nbsp;|&nbsp; All
+                    rights Reserved
+                </p>
+                <p className="w-6/12 text-right">
+                    <a
+                        href="legal/terms-and-conditions"
+                        className="hover:text-[#ED7423]"
+                    >
+                        Terms and Conditions
+                    </a>
+                    &nbsp; | &nbsp;
+                    <a
+                        href="legal/privacy-policy"
+                        className="hover:text-[#ED7423]"
+                    >
+                        Privacy Policy
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
