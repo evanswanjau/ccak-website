a251fd420896be9f10091b44dc1332e25e6ca14e
Evans Wanjau <evanswanjau@gmail.com>
2023-03-08 09:22:46 +0300

    Updated recentNews mobile responsiveness

--- src/Containers/recentNews.jsx
+++ src/Containers/recentNews.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { News } from "../Components/News";
+import { Fade } from "react-reveal";
 
 export const RecentNews = () => {
     const data = [
@@ -35,11 +36,16 @@ export const RecentNews = () => {
 
     return (
         <div className="py-5">
-            <div className="flex flex-row px-16">
-                <h2 className="w-2/12 text-2xl text-black font-bold">Recent News</h2>
-                <div className="w-10/12 border-t border-gray-200 mt-4"></div>
-            </div>
-            <div className="grid grid-cols-4 gap-6 px-16 py-10">
+            <Fade top>
+                <div className="flex flex-row px-6 lg:px-16">
+                    <h2 className="w-full lg:w-2/12 text-2xl text-left text-black font-bold">
+                        Recent News
+                    </h2>
+                    <div className="hidden md:block w-10/12 border-t border-gray-200 mt-4"></div>
+                </div>
+            </Fade>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 sm:gap-6 px-6 lg:px-16 py-10">
                 {data.map((item) => {
                     return <News data={item} />;
                 })}
