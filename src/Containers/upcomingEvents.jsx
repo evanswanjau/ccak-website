c53155e76344ff97a5697380bad7590ae68308c5
Evans Wanjau <evanswanjau@gmail.com>
2023-03-08 16:04:00 +0300

    Made upcoming events container responsive

--- src/Containers/upcomingEvents.jsx
+++ src/Containers/upcomingEvents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Event } from "../Components/event";
+import { Fade } from "react-reveal";
 
 const data = [
     {
@@ -31,13 +32,13 @@ const data = [
 export const UpcomingEvents = () => {
     return (
         <div className="py-5">
-            <div className="flex flex-row px-16">
-                <h2 className="w-3/12 text-2xl text-black font-bold">
+            <div className="flex flex-row px-6 lg:px-16">
+                <h2 className="w-full lg:w-3/12 text-2xl text-black font-bold">
                     Upcoming Events
                 </h2>
-                <div className="w-9/12 border-t border-gray-200 mt-4"></div>
+                <div className="hidden md:block w-9/12 border-t border-gray-200 mt-4"></div>
             </div>
-            <div className="grid grid-cols-4 gap-6 px-16 py-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 sm:gap-6 px-6 lg:px-16 py-10">
                 {data.map((item) => {
                     return <Event data={item} />;
                 })}
