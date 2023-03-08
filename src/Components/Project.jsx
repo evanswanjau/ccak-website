9920e0e2e2b8732e8c66c255bcacbb6f9664495d
Evans Wanjau <evanswanjau@gmail.com>
2023-03-08 17:31:46 +0300

    Updated project component UI

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,12 +1,13 @@
-export const Project = ({ data: { title, description, image, link } }) => {
+export const Project = ({ data: { title, description, image, url } }) => {
     return (
-        <a
-            href={`/projects/${link}`}
-            className={`bg-[url('${image}')] bg-cover bg-center bg-no-repeat rounded-lg text-white`}
-        >
-            <div className="h-full w-full backdrop-brightness-50 pt-32 p-5 rounded-lg">
-                <h3 className="text-xl my-3">{title}</h3>
-                <p>{description}</p>
+        <a href={`/media-centre/projects/${url}`}>
+            <div
+                className={`bg-[url('${image}')] bg-cover bg-center bg-no-repeat rounded-lg text-white group`}
+            >
+                <div className="h-full w-full bg-gradient-to-t from-black to-transparent px-3 pb-3 pt-28 rounded-lg group-hover:text-[#ED7423] transition duration-300 ease-in-out">
+                    <h3 className="font-semibold mb-1">{title}</h3>
+                    <p>{description}</p>
+                </div>
             </div>
         </a>
     );
