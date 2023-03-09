2867adc3bbeda9f7469195c0390d63539e03cdc1
Evans Wanjau <evanswanjau@gmail.com>
2023-03-09 19:56:11 +0300

    Added ease in transition on text

--- src/Components/member.jsx
+++ src/Components/member.jsx
@@ -17,10 +17,10 @@ export const Member = ({ data: { name, position, image, bio }, setMember }) => {
                     className="w-38 h-38 rounded-lg mb-6 mx-auto cursor-pointer hover:scale-[1.05] duration-300 ease-in-out"
                     src="/avatar.png"
                 ></img>
-                <h6 className="text font-semibold capitalize group-hover:text-[#ED7423]">
+                <h6 className="text font-semibold capitalize group-hover:text-[#ED7423] duration-300 ease-in-out">
                     {name}
                 </h6>
-                <p className="text-gray-600 capitalize group-hover:text-[#ED7423]">
+                <p className="text-gray-600 capitalize group-hover:text-[#ED7423] duration-300 ease-in-out">
                     {position}
                 </p>
             </div>
