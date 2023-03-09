a669e7d62b6197e654ce4de95d074aeca23c7c31
Evans Wanjau <evanswanjau@gmail.com>
2023-03-09 19:43:12 +0300

    Added member component

---
+++ src/Components/member.jsx
@@ -0,0 +1,29 @@
+import { Slide } from "react-reveal";
+import { MemberModal } from "./memberModal";
+
+export const Member = ({ data: { name, position, image, bio }, setMember }) => {
+    return (
+        <Slide bottom>
+            <div
+                className="flex flex-col justify-items-center group"
+                onClick={() => {
+                    setMember({
+                        modal: true,
+                        member: { name, position, image, bio },
+                    });
+                }}
+            >
+                <img
+                    className="w-38 h-38 rounded-lg mb-6 mx-auto cursor-pointer hover:scale-[1.05] duration-300 ease-in-out"
+                    src="/avatar.png"
+                ></img>
+                <h6 className="text font-semibold capitalize group-hover:text-[#ED7423]">
+                    {name}
+                </h6>
+                <p className="text-gray-600 capitalize group-hover:text-[#ED7423]">
+                    {position}
+                </p>
+            </div>
+        </Slide>
+    );
+};
