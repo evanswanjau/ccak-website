9464040e442c0b3e4ec6e48755b4a39828d8d3c8
Evans Wanjau <evanswanjau@gmail.com>
2023-03-10 20:13:00 +0300

    Moved media pages into folder

---
+++ src/Pages/media-centre/news.jsx
@@ -0,0 +1,123 @@
+import { News } from "../../Components/News";
+import { MediaCentreSideBar } from "../../Containers/mediaCentreSideBar";
+import { UpcomingEvents } from "../../Containers/upcomingEvents";
+import { Slide } from "react-reveal";
+
+export const NewsPage = () => {
+    const data = [
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+        {
+            title: "Don’t destroy greenery and don’t spoil scenery",
+            published: "February 20, 2023",
+            excerpt: "Lorem ipsum dolor sit amet, conser adipiscing elit...",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "url",
+        },
+    ];
+
+    return (
+        <div>
+            <section className="text-center py-12">
+                <Slide bottom>
+                    <h1 className="text-4xl font-semibold my-5">News Page</h1>
+                </Slide>
+                <Slide bottom>
+                    <p className="w-full px-5 md:w-6/12 mx-auto">
+                        Welcome to our news page. Here you will find a variety
+                        of news articles covering topics such as product
+                        innovations, industry trends, company events, and more.
+                    </p>
+                </Slide>
+            </section>
+            <div className="flex flex-row px-6 lg:px-16">
+                <div className="w-full lg:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 sm:gap-6 py-10">
+                    {data.map((item) => {
+                        return <News data={item} />;
+                    })}
+                </div>
+                <div className="hidden lg:block w-4/12 pt-10 pl-10">
+                    <MediaCentreSideBar category="press release" />
+                </div>
+            </div>
+            <section>
+                <UpcomingEvents />
+            </section>
+        </div>
+    );
+};
---
+++ src/Pages/media-centre/projects.jsx
@@ -0,0 +1,95 @@
+import { Project } from "../../Components/Project";
+import { MediaCentreSideBar } from "../../Containers/mediaCentreSideBar";
+import { RecentNews } from "../../Containers/recentNews";
+import { Slide } from "react-reveal";
+
+export const ProjectsPage = () => {
+    const data = [
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+        {
+            title: "Mission 40K: Tree plantation",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image: "http://localhost:5173/src/assets/home/BGHomeVid.png",
+            url: "mission-40k",
+        },
+    ];
+
+    return (
+        <div>
+            <section className="text-center py-12">
+                <Slide bottom>
+                    <h1 className="text-4xl font-semibold my-5">Projects</h1>
+                </Slide>
+                <Slide bottom>
+                    <p className="w-full px-5 md:w-6/12 mx-auto">
+                        Welcome to our projects page! This is where we showcase
+                        some of the exciting projects we're currently working on
+                        and those we've completed in the past.
+                    </p>
+                </Slide>
+            </section>
+            <div className="flex flex-row px-6 lg:px-16">
+                <div className="w-full lg:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 sm:gap-6 py-10">
+                    {data.map((item) => {
+                        return <Project data={item} />;
+                    })}
+                </div>
+                <div className="hidden lg:block w-4/12 pt-10 pl-10">
+                    <MediaCentreSideBar category="press release" />
+                </div>
+            </div>
+            <section>
+                <RecentNews />
+            </section>
+        </div>
+    );
+};
