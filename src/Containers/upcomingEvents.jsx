bfb59dd79d81b19473fc23b6474b8a4a504b4528
Evans Wanjau <evanswanjau@gmail.com>
2023-03-10 11:35:50 +0300

    Added fade effect on title

--- src/Containers/upcomingEvents.jsx
+++ src/Containers/upcomingEvents.jsx
@@ -32,12 +32,15 @@ const data = [
 export const UpcomingEvents = () => {
     return (
         <div className="py-5">
-            <div className="flex flex-row px-6 lg:px-16">
-                <h2 className="w-full lg:w-3/12 text-2xl text-black font-bold">
-                    Upcoming Events
-                </h2>
-                <div className="hidden md:block w-9/12 border-t border-gray-200 mt-4"></div>
-            </div>
+            <Fade top>
+                <div className="flex flex-row px-6 lg:px-16">
+                    <h2 className="w-full lg:w-3/12 text-2xl text-black font-bold">
+                        Upcoming Events
+                    </h2>
+                    <div className="hidden md:block w-9/12 border-t border-gray-200 mt-4"></div>
+                </div>
+            </Fade>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 sm:gap-6 px-6 lg:px-16 py-10">
                 {data.map((item) => {
                     return <Event data={item} />;
