1be9f6e0ac57bb9643074e315cc58568dd8372de
Evans Wanjau <evanswanjau@gmail.com>
2023-03-15 21:39:55 +0300

    Updated menuBar component on responsiveness and search bar

--- src/Components/navBar/menuBar.jsx
+++ src/Components/navBar/menuBar.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { magnifyingGlass } from "@heroicons/react/24/solid";
 
 export const MenuBar = ({ setDropDown }) => {
     const links = [
@@ -27,32 +25,38 @@ export const MenuBar = ({ setDropDown }) => {
     ];
 
     return (
-        <div className="flex flex-wrap pt-7 px-5 pb-5 w-100 font-manjari text-gray-600 bg-[#329E49]">
-            <div className="w-9/12">
+        <div className="flex flex-row items-center py-5 px-5 w-100 font-manjari text-gray-600 bg-[#329E49]">
+            <div className="lg:w-10/12 pt-1">
                 <ul className="flex flex-row">
-                    {links.map((link) => (
-                        <a href={link.link}>
+                    {links.map((link) =>
+                        link.link === "#" ? (
                             <li
-                                className="px-5 cursor-pointer text-lg text-white hover:text-[#ED7423]"
+                                className="px-5 cursor-pointer truncate text-lg text-gray-100 hover:text-white"
                                 onClick={() => {
-                                    link.link === "#" &&
-                                        setDropDown({
-                                            show: true,
-                                            category: link.name,
-                                        });
+                                    setDropDown({
+                                        show: true,
+                                        category: link.name,
+                                    });
                                 }}
                             >
                                 {link.name}
                             </li>
-                        </a>
-                    ))}
+                        ) : (
+                            <a href={link.link}>
+                                <li className="px-5 cursor-pointer truncate text-lg text-gray-100 hover:text-white">
+                                    {link.name}
+                                </li>
+                            </a>
+                        )
+                    )}
                 </ul>
             </div>
-            <div className="w-3/12">
-                <magnifyingGlass />
-                {/* <Link to="/">
-                    <img className="mx-5" src={logo} alt="CCAK Logo" />
-                </Link> */}
+            <div className="lg:w-2/12">
+                <input
+                    className="w-full placeholder-gray-300 bg-[#2e8641] text-white font-manjari outline-0 rounded-md pt-2 pb-1 px-3"
+                    type="text"
+                    placeholder={`Search`}
+                />
             </div>
         </div>
     );
