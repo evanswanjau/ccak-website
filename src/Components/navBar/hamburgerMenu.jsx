67faf3f58adb05226e8e35201633d5c60fc219b3
Evans Wanjau <evanswanjau@gmail.com>
2023-03-15 23:49:23 +0300

    Added responsive menus

---
+++ src/Components/navBar/hamburgerMenu.jsx
@@ -0,0 +1,23 @@
+export const HamburgerMenu = ({ setMobile }) => {
+    return (
+        <div className="w-full flex justify-between items-center py-2 px-6 bg-white shadow-sm">
+            <a href="/">
+                <img
+                    className="w-32"
+                    src="/ccak_side_logo.png"
+                    alt="CCAK Logo"
+                />
+            </a>
+            <div
+                className="flex flex-col space-y-1 cursor-pointer"
+                onClick={() => {
+                    setMobile(true);
+                }}
+            >
+                {[1, 2, 3].map(() => {
+                    return <span className="w-7 border-2 border-black rounded-sm"></span>;
+                })}
+            </div>
+        </div>
+    );
+};
