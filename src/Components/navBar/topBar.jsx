ee9852936cd2b756a296162f4a04663ee909602b
Evans Wanjau <evanswanjau@gmail.com>
2023-03-15 20:53:38 +0300

    Updated topBar links

--- src/Components/navBar/topBar.jsx
+++ src/Components/navBar/topBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/Logoblack.svg";
 
 export const TopBar = ({ setDropDown }) => {
     const links = [
@@ -15,33 +14,47 @@ export const TopBar = ({ setDropDown }) => {
     ];
 
     return (
-        <div className="flex flex-wrap p-2 w-100 font-manjari text-gray-600">
+        <nav className="flex flex-row p-2 w-100 font-manjari text-gray-600 bg-white items-centre lg:px-16">
             <div className="w-3/12">
-                <Link to="/">
-                    <img className="mx-5" src={logo} alt="CCAK Logo" />
-                </Link>
+                <a href="/">
+                    <img
+                        className="w-28"
+                        src="/ccak_side_logo.png"
+                        alt="CCAK Logo"
+                    />
+                </a>
             </div>
             <div className="w-9/12">
                 <ul className="flex flex-row justify-end items-center">
-                    {links.map((link) => (
-                        <a href={link.link}>
+                    {links.map((link) =>
+                        link.link === "#" ? (
                             <li
                                 className={` ${
                                     link.name !== "RESOURCE CENTRE" &&
                                     "border-l"
                                 }  px-5 cursor-pointer hover:text-[#ED7423]`}
                                 onClick={() => {
-                                    link.link === "#" &&
-                                        setDropDown({
-                                            show: true,
-                                            category: link.name,
-                                        });
+                                    setDropDown({
+                                        show: true,
+                                        category: link.name,
+                                    });
                                 }}
                             >
                                 {link.name}
                             </li>
-                        </a>
-                    ))}
+                        ) : (
+                            <a href={link.link}>
+                                <li
+                                    className={` ${
+                                        link.name !== "RESOURCE CENTRE" &&
+                                        "border-l"
+                                    }  px-5 cursor-pointer hover:text-[#ED7423]`}
+                                >
+                                    {link.name}
+                                </li>
+                            </a>
+                        )
+                    )}
                     <Link to="/donate">
                         <button className="bg-[#329E49] text-white pt-2 pb-1 px-5 mx-5 hover:bg-[#3ab554] rounded-md tracking-widest">
                             DONATE
@@ -49,6 +62,6 @@ export const TopBar = ({ setDropDown }) => {
                     </Link>
                 </ul>
             </div>
-        </div>
+        </nav>
     );
 };
