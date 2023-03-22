13b40919f145bfe695236bf504e4d14ea20b7fe8
Evans Wanjau <evanswanjau@gmail.com>
2023-03-22 17:14:43 +0300

    Added date helper that formats date

---
+++ src/helpers/date.js
@@ -0,0 +1,21 @@
+module.exports.simpleDate = (date) => {
+    let newDate = new Date(date).toString();
+
+    newDate = newDate.split(" ");
+
+    return newDate[2].replace(/^0+/, '') + nth(newDate[2]) + " " + newDate[1] + " " + newDate[3];
+};
+
+const nth = (d) => {
+    if (d > 3 && d < 21) return "th";
+    switch (d % 10) {
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        case 3:
+            return "rd";
+        default:
+            return "th";
+    }
+};
