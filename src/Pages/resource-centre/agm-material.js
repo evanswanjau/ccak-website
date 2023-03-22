45f015e72d7405a8cf902c57082bda6a01c8ea38
Evans Wanjau <evanswanjau@gmail.com>
2023-03-22 21:56:25 +0300

    Updated sample data and component varaibles

--- src/Pages/resource-centre/agm-material.js
+++ src/Pages/resource-centre/agm-material.js
@@ -4,64 +4,64 @@ import { Research } from "../../Components/research";
 const data = [
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
     {
         title: "Analysis of the implications of the value-added tax on clean cooking in Kenya",
-        description:
+        excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "mission-40k",
-        published: "February 20, 2023",
+        published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
@@ -84,7 +84,7 @@ export const AgmMaterialPage = () => {
                     </p>
                 </Slide>
                 <div className="flex flex-row px-6 lg:px-16 py-5">
-                    <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-y-6 sm:gap-6 py-10">
+                    <div className="w-full grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-y-6 sm:gap-6 py-10">
                         {data.map((item, i) => {
                             return <Research key={i} data={item} />;
                         })}
