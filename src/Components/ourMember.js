9f31fc42c49bb7a2c007800f053039e7fb4a46ca
Evans Wanjau <evanswanjau@gmail.com>
2023-03-23 13:11:35 +0300

    Added our members page and ourMember component

---
+++ src/Components/ourMember.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import {
+    UserCircleIcon,
+    PhoneIcon,
+    MapPinIcon,
+} from "@heroicons/react/24/outline";
+import { Fade } from "react-reveal";
+
+export const OurMember = ({
+    data: { name, category, technology, phone_number, location },
+}) => {
+    return (
+        <Fade bottom>
+            <div className="bg-[#EFF7F2] p-6 rounded-lg text-left">
+                <div className="flex mb-5">
+                    <div className="w-2/12">
+                        <UserCircleIcon className="w-12" />
+                    </div>
+                    <div className="w-10/12">
+                        <h4 className="capitalize font-semibold text-2xl">
+                            {name}
+                        </h4>
+                        <p className="uppercase text-sm">{category}</p>
+                    </div>
+                </div>
+                <div className="space-y-2">
+                    <p className="my-2 capitalize">
+                        <b>Technology:</b> {technology}
+                    </p>
+                    <a href={`tel:${phone_number}`}>
+                        <p className="flex">
+                            <PhoneIcon className="w-5 mr-3 -mt-1" />
+                            {phone_number}
+                        </p>
+                    </a>
+                    <p className="flex">
+                        <MapPinIcon className="w-5 mr-3 -mt-1" />
+                        {location}
+                    </p>
+                </div>
+            </div>
+        </Fade>
+    );
+};
---
+++ src/Pages/membership/our-members.js
@@ -0,0 +1,160 @@
+import { Slide, Fade } from "react-reveal";
+import { OurMember } from "../../Components/ourMember";
+import { Link } from "react-router-dom";
+
+const data = [
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+    {
+        name: "burn",
+        category: "corporate large",
+        technology: "biomass cookstoves",
+        phone_number: "0706585629",
+        location: "New Horizons Industrial Park, Ruiru Kenya",
+    },
+];
+
+let technologies = [
+    "associate-members",
+    "biogas",
+    "biomass",
+    "eletricity-and-lpg",
+    "ethanol",
+    "solar",
+];
+
+let categories = [
+    "corporate-large",
+    "corporate-medium",
+    "corporate-small",
+    "corporate-micro",
+    "ngo-international",
+    "ngo-local",
+    "donor-large",
+    "asoociation-membership",
+    "CSO",
+    "intitution-research,technology",
+    "individual",
+    "students",
+];
+
+export const OurMembersPage = () => {
+    return (
+        <div className="pt-[4rem] lg:pt-[8.2rem]">
+            <section className="text-center py-12">
+                <Slide bottom>
+                    <h1 className="text-4xl font-semibold my-5">Members</h1>
+                </Slide>
+                <Slide bottom>
+                    <p className="w-full text-gray-600 px-6 lg:w-6/12 mx-auto">
+                        Our members are committed to promoting adoption of clean
+                        cooking technologies, capacity building of the sector,
+                        and sector coordination, and work together towards an
+                        enabling environment at both national and county levels
+                        to catalyze the growth of the clean cooking sector.
+                    </p>
+                </Slide>
+            </section>
+            <div className="flex flex-row px-6 lg:px-16">
+                <div className="w-full lg:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-y-6 sm:gap-6 py-10">
+                    {data.map((item, i) => {
+                        return <OurMember key={i} data={item} />;
+                    })}
+                </div>
+                <div className="hidden lg:block w-4/12 pt-10 pl-10">
+                    <Fade>
+                        <input
+                            className="w-full bg-gray-100 mb-5 font-manjari outline-0 rounded-md pt-3 pb-2 px-3"
+                            type="text"
+                            placeholder={`Search member`}
+                        />
+                    </Fade>
+                    <div className="bg-[#F3F3F3] p-5 rounded-lg my-5">
+                        <h3 className="border-b pb-2">TECHNOLOGY</h3>
+                        <ul className="font-manjari space-y-2 my-5">
+                            {technologies.map((technology) => {
+                                return (
+                                    <Link to={technology}>
+                                        <li className="hover:text-[#ED7423] transition duration-150 ease-in-out">
+                                            {technology
+                                                .charAt(0)
+                                                .toUpperCase() +
+                                                technology
+                                                    .slice(1)
+                                                    .replace(/-/g, " ")}
+                                        </li>
+                                    </Link>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <div className="bg-[#F3F3F3] p-5 rounded-lg my-5">
+                        <h3 className="border-b pb-2">CATEGORY</h3>
+                        <ul className="font-manjari space-y-2 my-5">
+                            {categories.map((category) => {
+                                return (
+                                    <Link to={category}>
+                                        <li className="hover:text-[#ED7423] transition duration-150 ease-in-out">
+                                            {category
+                                                .charAt(0)
+                                                .toUpperCase() +
+                                                category
+                                                    .slice(1)}
+                                        </li>
+                                    </Link>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
