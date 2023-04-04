580fed5386f043f6475002be6d94f7681b1eb8d1
Evans Wanjau <evanswanjau@gmail.com>
2023-04-04 07:44:52 +0300

    update: updated text color to theme color for current menu item

--- src/Components/navBar/socialHub/sideMenu.js
+++ src/Components/navBar/socialHub/sideMenu.js
@@ -6,8 +6,16 @@ import {
     HomeIcon,
 } from "@heroicons/react/24/outline";
 const lists = [
-    { name: "Home", link: "/social-hub/home", icon: <HomeIcon className="w-6 -mt-1" /> },
-    { name: "Profile", link: "/social-hub/profile", icon: <UserIcon className="w-6 -mt-1" /> },
+    {
+        name: "Home",
+        link: "/social-hub/home",
+        icon: <HomeIcon className="w-6 -mt-1" />,
+    },
+    {
+        name: "Profile",
+        link: "/social-hub/profile",
+        icon: <UserIcon className="w-6 -mt-1" />,
+    },
     {
         name: "Bookmarks",
         link: "/social-hub/bookmarks",
@@ -26,6 +34,7 @@ const lists = [
 ];
 
 export const SideMenu = () => {
+    console.log(window.location.pathname.split("/")[2]);
     return (
         <div className="p-10 rounded-lg shadow-lg">
             {lists.map((list, i) => {
@@ -34,7 +43,12 @@ export const SideMenu = () => {
                         href={list.link}
                         className={`flex py-5 space-x-4 ${
                             i + 1 < lists.length && "border-b border-gray-100"
-                        } text-gray-600 hover:text-[#ED7423] transition duration-300 ease-in-out`}
+                        } ${
+                            window.location.pathname.split("/")[2] ===
+                            list.link.split("/")[2]
+                                ? "text-[#329E49]"
+                                : "text-gray-600"
+                        } hover:text-[#329E49] transition duration-300 ease-in-out`}
                     >
                         {list.icon}
                         <p>{list.name}</p>
