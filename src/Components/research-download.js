e6e030a278252d321166ef5e3ebf2c18bc344f4e
Evans Wanjau <evanswanjau@gmail.com>
2023-04-22 06:03:31 +0300

    new: added new resources download component

---
+++ src/Components/research-download.js
@@ -0,0 +1,26 @@
+import { Fade } from "react-reveal";
+
+export const ResearchDownload = ({
+    data: { title, excerpt, published, url, category, size },
+}) => {
+    return (
+        <Fade>
+            <div className="bg-[#EFF7F2] text-left p-5 rounded-lg">
+                <div className="flex w-full pb-3 group-hover:border-white transition duration-150 ease-in-out">
+                    <h3 className="font-semibold mb-1 group-hover:text-white transition duration-150 ease-in-out">
+                        {title}
+                    </h3>
+                </div>
+                <div className="flex flex-row justify-between items-center mt-3">
+                    <button
+                        href="#"
+                        className="bg-white font-manjari leading-none pb-2 text-md pt-3 px-5 hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest"
+                    >
+                        DOWNLOAD
+                    </button>
+                    <p>{size}</p>
+                </div>
+            </div>
+        </Fade>
+    );
+};
