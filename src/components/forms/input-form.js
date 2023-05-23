0f096313e0ba9582787a50136259f2af1e2e02aa
Evans Wanjau <evanswanjau@gmail.com>
2023-05-23 10:02:36 +0300

    update: updated input form to include password inputs

--- src/components/forms/input-form.js
+++ src/components/forms/input-form.js
@@ -1,22 +1,58 @@
-export const InputForm = ({ type, name, label, data, updateData }) => {
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+export const InputForm = ({
+    type,
+    name,
+    label,
+    required = false,
+    data,
+    updateData,
+}) => {
+    const [pass, setPass] = useState(false);
+    const [passType, setType] = useState(type);
     return (
         <div>
             <label
                 for={type}
-                class="block mb-2 text-sm font-medium text-gray-900 font-manjari"
+                class="block uppercase mb-2 text-[0.7em] font-bold text-gray-900 font-montserrat"
             >
-                {label}
+                {label}{" "}
+                {required && <span className="text-red-600 text-sm">*</span>}
             </label>
-            <input
-                type={type}
-                id={name}
-                class="bg-gray-50 border-2 border-transparent font-manjari text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-[#ED7423] focus:border-[#ED7423] block w-full p-2.5 "
-                placeholder={`Enter ${label.toLowerCase()}`}
-                value={data[name]}
-                onChange={(event) => {
-                    updateData({ ...data, [name]: event.target.value });
-                }}
-            />
+            <div className="relative">
+                <input
+                    type={type === "password" && passType}
+                    id={name}
+                    class="border font-manjari text-gray-900 rounded-lg focus:outline-none focus:ring-[#ED7423] focus:border-[#ED7423] block w-full p-2 pt-3"
+                    placeholder={label.toLowerCase()}
+                    value={data[name]}
+                    onChange={(event) => {
+                        updateData({ ...data, [name]: event.target.value });
+                    }}
+                />
+                {type === "password" && (
+                    <div>
+                        {pass ? (
+                            <EyeIcon
+                                className="w-6 absolute top-3 right-2 text-gray-400 cursor-pointer"
+                                onClick={() => {
+                                    setType("password");
+                                    setPass(false);
+                                }}
+                            />
+                        ) : (
+                            <EyeSlashIcon
+                                className="w-6 absolute top-3 right-2 text-gray-400 cursor-pointer"
+                                onClick={() => {
+                                    setType("text");
+                                    setPass(true);
+                                }}
+                            />
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
