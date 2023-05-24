b08c8bc00253833f06c925c1f04be49c2eb00bb5
Evans Wanjau <evanswanjau@gmail.com>
2023-05-24 12:25:04 +0300

    update: updated resource centre with publications and agm materials

--- src/pages/resource-centre/funding-opportunities.js
+++ src/pages/resource-centre/funding-opportunities.js
@@ -7,7 +7,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "1/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -17,7 +17,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "2/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -27,7 +27,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "3/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -37,7 +37,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "4/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -47,7 +47,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "5/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -57,7 +57,7 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "6/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
@@ -67,27 +67,27 @@ const data = [
         excerpt:
             "A key policy objective of the Government of Kenya (GoK) is to meet the Sustainable Development Goal (SDG) 7 target of universal access to clean cooking solutions by 2028.",
         url: "7/analysis-of-the-implications-of-the-value-added-tax-on-clean-cooking-in-kenya",
-        category: "research-papers",
+        category: "agm-material",
         published: "2023-03-08",
         downloads: 23,
         size: "136.6 kb",
     },
 ];
 
-export const ResearchPapersPage = () => {
+export const FundingOpportunitiesRCPage = () => {
     return (
         <div className="pt-[4rem] lg:pt-[8.2rem]">
             <section className="text-center py-12">
                 <Slide bottom>
                     <h1 className="text-4xl font-semibold my-5">
-                        Research Papers
+                        Funding Opportunities
                     </h1>
                 </Slide>
                 <Slide bottom>
                     <p className="w-full text-gray-600 px-6 lg:w-6/12 mx-auto">
-                        Welcome to our research papers page! This is the place
-                        where you can find a collection of our latest and most
-                        innovative research in a variety of fields.
+                        Welcome to the Funding Opportunities page! On this page, we will provide
+                        you with a comprehensive guide to everything you need to
+                        know about funding opportunities.
                     </p>
                 </Slide>
                 <div className="flex flex-row px-6 lg:px-16 py-5">
