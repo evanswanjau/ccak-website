3b652f5a751f293d8bc2121a2cd9832c1eec7d46
Evans Wanjau <evanswanjau@gmail.com>
2023-05-25 16:35:30 +0300

    new: added register now api call

--- src/api/api-calls.js
+++ src/api/api-calls.js
@@ -12,3 +12,17 @@ export const apiRequest = (method, url, data, updateData) => {
         updateData(data);
     });
 };
+
+export const registerMember = (data) => {
+    return axios({
+        method: "post",
+        url: process.env.REACT_APP_API_URL + "member",
+        data: data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(({ data }) => {
+        window.location.replace("/");
+        console.log(data);
+    });
+};
