3bd131e9c86a9b8e5cb525dc0f426b321d482b3d
Evans Wanjau <evanswanjau@gmail.com>
2023-05-25 10:30:58 +0300

    update: redesigned footer to make it smaller and changed background to light gray

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,109 +1,54 @@
-import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { InputForm } from "./forms/input-form";
 
 export const Footer = () => {
+    const [data, updateData] = useState({ email: "" });
     const links = [
+        { name: "About Us", url: "/who-we-are/about-us" },
         {
-            name: "WHO WE ARE",
-            links: [
-                { name: "About Us", url: "/who-we-are/about-us" },
-                {
-                    name: "Our Mission",
-                    url: "/who-we-are/about-us#our-mission",
-                },
-                { name: "Our Vision", url: "/who-we-are/about-us#our-vision" },
-                {
-                    name: "Our Partners",
-                    url: "/who-we-are/about-us#our-partners",
-                },
-                { name: "Our Team", url: "/who-we-are/our-team" },
-            ],
+            name: "Our Mission",
+            url: "/who-we-are/about-us#our-mission",
         },
+        { name: "Our Vision", url: "/who-we-are/about-us#our-vision" },
         {
-            name: "GET INVOLVED",
-            links: [
-                { name: "Careers", url: "/get-involved/careers" },
-                { name: "Consultancy", url: "/get-involved/consultancy" },
-                { name: "Donate", url: "/get-involved/donate" },
-                { name: "Funding", url: "/get-involved/funding-opportunities" },
-                { name: "Contact Us", url: "/get-involved/contact-us" },
-            ],
+            name: "Our Partners",
+            url: "/who-we-are/about-us#our-partners",
         },
-        {
-            name: "MEDIA CENTRE",
-            links: [
-                { name: "News", url: "/media-centre/news" },
-                { name: "Events", url: "/media-centre/events" },
-                { name: "Projects", url: "/media-centre/projects" },
-                { name: "Press", url: "/media-centre/press-release" },
-                { name: "Gallery", url: "/media-centre/photo-gallery" },
-            ],
-        },
+        { name: "Our Team", url: "/who-we-are/our-team" },
     ];
 
     return (
-        <footer className="w-full px-5 lg:px-10 pt-5 text-gray-200 bg-zinc-800">
+        <footer className="w-full px-5 md:px-10 pt-5 text-gray-800 bg-gray-50">
             <div className="flex flex-col lg:flex-row lg:space-x-8">
-                <div className="w-full lg:w-3/12">
+                <div className="w-full lg:w-8/12 flex flex-col">
                     <a href="/">
                         <img
-                            className="w-40 mb-5"
+                            className="w-40 mb-8"
                             src="/ccak_side_logo.png"
                             alt="CCAK Logo"
                         />
                     </a>
-                    <p className="py-3">
-                        The Clean Cooking Association of Kenya is a sector
-                        association with a vision to see universal access and
-                        adoption of clean cooking solutions and practices in
-                        kenya, through a sustainable market environment.
-                    </p>
-                    <div className="flex flex-row space-x-4 hover:text-[#ED7423] transition duration-300 ease-in-out my-2">
-                        <EnvelopeIcon className="w-5 h-5" />
-                        <a href="mailto:info@ccak.or.ke">info@ccak.or.ke</a>
-                    </div>
-                    <div className="flex flex-row space-x-4 hover:text-[#ED7423] transition duration-300 ease-in-out my-3">
-                        <PhoneIcon className="w-5 h-5" />
-                        <a href="tel:+254707111669">0707 111 669</a>
-                    </div>
-                </div>
-                <div className="w-full lg:w-6/12 pt-5">
-                    <div className="flex flex-col lg:flex-row justify-around">
-                        {links.map((link, i) => {
+                    <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 list-none">
+                        {links.map((item, i) => {
                             return (
-                                <div key={i}>
-                                    <h3 className="text-white font-medium my-5">
-                                        {link.name}
-                                    </h3>
-                                    <ul>
-                                        {link.links.map((item, i) => {
-                                            return (
-                                                <a href={item.url} key={i}>
-                                                    <li className="text-gray-200 hover:text-[#ED7423] transition duration-300 ease-in-out my-4">
-                                                        {item.name}
-                                                    </li>
-                                                </a>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
+                                <a href={item.url} key={i}>
+                                    <li className="text-gray-800 hover:text-[#ED7423] transition duration-300 ease-in-out">
+                                        {item.name}
+                                    </li>
+                                </a>
                             );
                         })}
-                    </div>
+                    </ul>
                 </div>
-                <div className="w-full lg:w-3/12 pt-5 font-manjari">
-                    <h3 className="text-gray-200 font-medium my-5">NEWSLETTER</h3>
-                    <p className="py-3">
-                        Stay up-to-date with the latest news and updates on
-                        clean cooking in Kenya by signing up for our newsletter!
-                        As a subscriber, you'll be the first to hear about our
-                        latest projects, events, and advocacy efforts.
-                    </p>
-                    <input
-                        className="w-full border border-1 outline-0 rounded-md pt-3 pb-2 px-3 focus:border-gray-400 focus:border-1"
-                        type="email"
-                        placeholder="Your email"
+                <div className="w-full lg:w-4/12 pt-5 font-manjari items-end flex space-x-4 lg:flex-row">
+                    <InputForm
+                        type="text"
+                        name="email"
+                        label="email address"
+                        data={data}
+                        updateData={updateData}
                     />
-                    <button className="my-3 bg-[#329E49] leading-none pb-2 pt-3 px-2 font-manjari text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest">
+                    <button className="bg-[#329E49] leading-none pb-3 pt-4 px-4 mt-5 font-manjari text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest">
                         SUBSCRIBE
                     </button>
                 </div>
