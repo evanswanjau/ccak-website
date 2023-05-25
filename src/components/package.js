4d7b12af5d2973a7558885469e5d43786ed38515
Evans Wanjau <evanswanjau@gmail.com>
2023-05-25 07:18:39 +0300

    update: updated packages page and package component ui

--- src/components/package.js
+++ src/components/package.js
@@ -4,8 +4,8 @@ import { Fade } from "react-reveal";
 export const Package = ({ data: { name, category, price, list } }) => {
     return (
         <Fade bottom>
-            <div className="bg-[#EFF7F2] p-6 lg:py-10 rounded-lg">
-                <h6 className="uppercase text-xs font-semibold text-gray-400">
+            <div className="bg-[#EFF7F2] py-6 px-3 lg:py-10 rounded-lg">
+                <h6 className="uppercase text-xl font-semibold text-gray-500">
                     {category}
                 </h6>
                 <h4 className="text-xl uppercase font-semibold">{name}</h4>
@@ -14,16 +14,20 @@ export const Package = ({ data: { name, category, price, list } }) => {
                 <ul className="text-left text-gray-600 my-5">
                     {list.map((item, i) => {
                         return (
-                            <li className="flex my-4 font-manjari">
-                                <PlayIcon className="w-1/12 mr-3 -mt-[5px] text-[#97DAAD]" />
+                            <li className="flex my-4 text-md font-manjari">
+                                <PlayIcon className="w-4 mr-3 -mt-[5px] text-[#97DAAD]" />
                                 <span className="w-11/12">{item}</span>
                             </li>
                         );
                     })}
                 </ul>
-                <button className="bg-[#329E49] font-manjari leading-none mt-5 pb-3 pt-4 px-5 text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest">
+                <br />
+                <a
+                    href="/membership/register"
+                    className="bg-[#329E49] font-manjari leading-none mt-10 pb-3 pt-4 px-5 text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest"
+                >
                     GET STARTED
-                </button>
+                </a>
             </div>
         </Fade>
     );
