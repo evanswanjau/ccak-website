3e72c889a9a391d70010b13dd33d912674286541
Evans Wanjau <evanswanjau@gmail.com>
2023-05-25 08:12:16 +0300

    update: updated CTA UI and content update

--- src/components/callToAction.js
+++ src/components/callToAction.js
@@ -10,15 +10,15 @@ export const CallToAction = () => {
                         "call_to_action_section_contribute_to_clean_cooking.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1678480594240"
                     })`,
                 }}
-                className="w-10/12 mb-10 mx-auto bg-cover bg-center bg-no-repeat rounded-lg"
+                className="flex mb-10 mx-6 lg:mx-16 bg-cover bg-center bg-no-repeat rounded-lg"
             >
-                <div className="bg-black/[.3] px-6 md:px-10 py-8 rounded-lg">
-                    <h3 className="text-white text-2xl sm:text-4xl text-center my-10 font-semibold leading-snug">
-                        You can contribute to make <br /> the environment
-                        greener!
+                <div className="bg-black/[.3] rounded-lg px-3 py-10 lg:p-24">
+                    <h3 style={{lineHeight:"50px"}} className="text-white text-2xl mx-auto lg:mb-10 lg:w-10/12 sm:text-4xl text-center font-semibold">
+                        You can contribute to the universal adoption of clean
+                        cooking in Kenya by 2028
                     </h3>
-                    <div className="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-6 pb-10">
-                        <a href="#">
+                    <div className="flex flex-row justify-center items-center my-5 space-x-6">
+                        <a href="/membership/register">
                             <button
                                 href="/get-involved/become-a-member"
                                 className="bg-[#329E49] font-manjari leading-none pb-3 pt-4 px-5 text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest"
