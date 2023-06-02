76fd6e39cda73f998d3d00c96d7d74e002d11692
Evans Wanjau <evanswanjau@gmail.com>
2023-06-02 20:49:05 +0300

    udpate: subscriber api integration

--- src/api/api-calls.js
+++ src/api/api-calls.js
@@ -13,6 +13,28 @@ export const searchPosts = (searchData, updateData) => {
     });
 };
 
+export const subscribeUser = (data, setSuccess, setError) => {
+    return axios({
+        method: "post",
+        url: process.env.REACT_APP_API_URL + "subscriber",
+        data: data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(({ data }) => {
+            setSuccess("You have been subscribed successfully");
+            setError(false)
+        })
+        .catch(({ response }) => {
+            let errors = response.data;
+            let keys = Object.keys(response.data);
+
+            setError(errors[keys[0]][0]);
+            setSuccess(false)
+        });
+};
+
 export const apiRequest = (method, url, data, updateData) => {
     return axios({
         method: method,
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 import { InputForm } from "./forms/input-form";
+import { subscribeUser } from "../api/api-calls";
+import { ErrorMessage } from "./forms/error";
+import { SuccessMessage } from "./forms/success";
 
 export const Footer = () => {
     const [data, updateData] = useState({ email: "" });
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
     const links = [
+        { name: "Home", url: "/" },
         { name: "About Us", url: "/who-we-are/about-us" },
+        { name: "What We Do", url: "/what-we-do" },
+        { name: "Events", url: "/media-centre/events" },
         {
-            name: "Our Mission",
-            url: "/who-we-are/about-us#our-mission",
+            name: "Donate",
+            url: "/get-involved/donate",
         },
-        { name: "Our Vision", url: "/who-we-are/about-us#our-vision" },
-        {
-            name: "Our Partners",
-            url: "/who-we-are/about-us#our-partners",
-        },
-        { name: "Our Team", url: "/who-we-are/our-team" },
+        { name: "Contact Us", url: "/get-involved/contact-us" },
     ];
 
+
     return (
-        <footer className="w-full px-5 md:px-10 pt-5 text-gray-800 bg-gray-50">
+        <footer className="w-full px-5 md:px-10 pt-5 text-gray-800 bg-[#ED7423] bg-opacity-30">
             <div className="flex flex-col lg:flex-row lg:space-x-8">
                 <div className="w-full lg:w-8/12 flex flex-col">
                     <a href="/">
@@ -28,7 +33,7 @@ export const Footer = () => {
                             alt="CCAK Logo"
                         />
                     </a>
-                    <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 list-none">
+                    <ul className="flex flex-col md:flex-row md:space-x-10 space-y-4 md:space-y-0 list-none">
                         {links.map((item, i) => {
                             return (
                                 <a href={item.url} key={i}>
@@ -40,20 +45,36 @@ export const Footer = () => {
                         })}
                     </ul>
                 </div>
-                <div className="w-full lg:w-4/12 pt-5 font-manjari items-end flex space-x-4 lg:flex-row">
-                    <InputForm
-                        type="text"
-                        name="email"
-                        label="email address"
-                        data={data}
-                        updateData={updateData}
-                    />
-                    <button className="bg-[#329E49] leading-none pb-3 pt-4 px-4 mt-5 font-manjari text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest">
-                        SUBSCRIBE
-                    </button>
+                <div className="w-full lg:w-4/12 pt-5 lg:pt-14 font-manjari items-end">
+                    {error && (
+                        <ErrorMessage error={error} setError={setError} />
+                    )}
+                    {success && (
+                        <SuccessMessage
+                            success={success}
+                            setSuccess={setSuccess}
+                        />
+                    )}
+                    <div className="w-full space-x-4 flex lg:flex-row">
+                        <InputForm
+                            type="text"
+                            name="email"
+                            label="email address"
+                            data={data}
+                            updateData={updateData}
+                        />
+                        <button
+                            className="bg-[#329E49] leading-none pb-3 pt-4 px-4 mt-5 font-manjari text-white hover:bg-[#ED7423] hover:text-white rounded-md transition duration-300 ease-in-out tracking-widest"
+                            onClick={() => {
+                                subscribeUser(data, setSuccess, setError);
+                            }}
+                        >
+                            SUBSCRIBE
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-col lg:flex-row border-t py-5 mt-12 text-sm">
+            <div className="flex flex-col lg:flex-row border-t border-gray-600 py-5 mt-12 text-sm">
                 <p className="w-full lg:w-6/12">
                     © Clean Cooking Association Kenya 2023 &nbsp;|&nbsp; All
                     rights Reserved
