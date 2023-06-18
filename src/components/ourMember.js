0be9e9bc6e4f1aaa4cf80ed7c071111c5310a443
Evans Wanjau <evanswanjau@gmail.com>
2023-06-19 02:24:41 +0300

    update: redesigned ui

--- src/components/ourMember.js
+++ src/components/ourMember.js
@@ -1,41 +1,80 @@
 import {
-    UserCircleIcon,
     PhoneIcon,
     MapPinIcon,
+    EnvelopeIcon,
+    GlobeAltIcon,
 } from "@heroicons/react/24/outline";
 import { Fade } from "react-reveal";
 
 export const OurMember = ({
-    data: { name, category, technology, phone_number, location },
+    data: {
+        company,
+        subscription_category,
+        technology,
+        company_phone,
+        company_email,
+        location,
+        logo,
+        website_link,
+    },
 }) => {
     return (
         <Fade bottom>
-            <div className="bg-[#F2F9F4] p-6 rounded-lg text-left flex flex-col max-h-[250px]">
+            <div className="bg-[#F2F9F4] p-6 rounded-lg text-left flex flex-col h-fit max-h-[350px]">
                 <div className="flex mb-5">
                     <div className="w-2/12">
-                        <UserCircleIcon className="w-12" />
+                        <img
+                            src={`${process.env.REACT_APP_IMAGEKIT_URL}members/${logo}`}
+                            alt={company}
+                            className=""
+                        />
                     </div>
-                    <div className="w-10/12">
+                    <div className="w-10/12 pl-2">
                         <h4 className="capitalize font-semibold text-2xl">
-                            {name}
+                            {company}
                         </h4>
-                        <p className="uppercase text-sm">{category}</p>
+                        <p className="uppercase text-sm">
+                            {subscription_category}
+                        </p>
                     </div>
                 </div>
                 <div className="space-y-2">
                     <p className="my-2 capitalize">
                         <b>Technology:</b> {technology}
                     </p>
-                    <a href={`tel:${phone_number}`}>
+                    <a
+                        href={`tel:${company_phone}`}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <p className="flex">
                             <PhoneIcon className="w-5 mr-3 -mt-1" />
-                            {phone_number}
+                            {company_phone}
+                        </p>
+                    </a>
+                    <a
+                        href={`mail:${company_email}`}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <p className="flex">
+                            <EnvelopeIcon className="w-5 mr-3 -mt-1" />
+                            {company_email}
                         </p>
                     </a>
                     <p className="flex">
                         <MapPinIcon className="w-5 mr-3 -mt-1" />
                         {location}
                     </p>
+                    <a
+                        href={website_link}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex"
+                    >
+                        <GlobeAltIcon className="w-5 mr-3 -mt-1" />
+                        {website_link}
+                    </a>
                 </div>
             </div>
         </Fade>
