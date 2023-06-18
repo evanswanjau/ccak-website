371992b89e9a1c3a555dc04c26da6786f3b96528
Evans Wanjau <evanswanjau@gmail.com>
2023-06-18 20:01:14 +0300

    new: added photo gallery page

---
+++ src/pages/read-more/photo-gallery.js
@@ -0,0 +1,67 @@
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import Gallery from "react-photo-gallery";
+import { apiRequest } from "../../api/api-calls";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+export const PhotoGalleryReadMore = () => {
+    const params = useParams();
+    const [photos, updatePhotos] = useState([]);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+    useEffect(() => {
+        let newArr = [];
+
+        apiRequest("get", "post/" + params.id, photos, updatePhotos)
+            .then((data) => {
+                data.files.data.map((image) => {
+                    return newArr.push({
+                        src:
+                            process.env.REACT_APP_IMAGEKIT_URL +
+                            data.folder +
+                            "/" +
+                            image,
+                        sizes: [
+                            "(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw",
+                        ],
+                        width: 4,
+                        height: 3,
+                    });
+                });
+            })
+            .finally(() => {
+                updatePhotos(newArr);
+            });
+    }, []); // eslint-disable-line
+
+    return (
+        <div className="pt-[3.8rem] lg:pt-[6.9rem]">
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map((x) => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title,
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    );
+};
