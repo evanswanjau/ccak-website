c2bc7ac15e7ed2ac6d89d1b4fac80a94909508a6
Evans Wanjau <evanswanjau@gmail.com>
2023-06-18 20:01:38 +0300

    fix: updated galery component ui

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,19 +1,30 @@
 import { Fade } from "react-reveal";
 
-export const Gallery = ({ data: { title, description, image, url } }) => {
+export const Gallery = ({ data: { id, title, excerpt, image, folder } }) => {
     return (
         <Fade>
-            <a href={`/media-centre/photo-gallery/${url}`}>
+            <a
+                href={`/media-centre/photo-gallery/read-more/${id}/${title
+                    .replace(/ /g, "-")
+                    .replace("'", "")
+                    .toLowerCase()}`}
+            >
                 <div
                     style={{
                         backgroundImage: `url(${
-                            process.env.REACT_APP_IMAGEKIT_URL + image
+                            process.env.REACT_APP_IMAGEKIT_URL +
+                            folder +
+                            "/" +
+                            image
                         })`,
                     }}
-                    className={`bg-cover bg-center bg-no-repeat rounded-lg text-white group`}
+                    className={`relative bg-cover bg-center bg-no-repeat rounded-lg text-white group flex flex-col min-h-[300px]`}
                 >
-                    <div className="h-full w-full bg-gradient-to-t from-black to-transparent px-3 pb-3 pt-36 rounded-lg group-hover:text-[#ED7423] transition duration-300 ease-in-out">
+                    <div className="absolute inset-0 flex flex-col justify-end items-end h-full w-full bg-gradient-to-t from-black to-transparent px-3 pb-3 pt-36 rounded-lg group-hover:text-[#ED7423] transition duration-300 ease-in-out">
                         <h3 className="font-semibold mb-1">{title}</h3>
+                        <p className="line-clamp-2" title={excerpt}>
+                            {excerpt}
+                        </p>
                     </div>
                 </div>
             </a>
