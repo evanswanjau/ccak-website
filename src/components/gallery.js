594a9eca6f3f7e6fddd4c648ce9fbfaf5b17ced8
Evans Wanjau <evanswanjau@gmail.com>
2023-06-19 02:24:52 +0300

    update: redesing ui

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -21,7 +21,7 @@ export const Gallery = ({ data: { id, title, excerpt, image, folder } }) => {
                     className={`relative bg-cover bg-center bg-no-repeat rounded-lg text-white group flex flex-col min-h-[300px]`}
                 >
                     <div className="absolute inset-0 flex flex-col justify-end items-end h-full w-full bg-gradient-to-t from-black to-transparent px-3 pb-3 pt-36 rounded-lg group-hover:text-[#ED7423] transition duration-300 ease-in-out">
-                        <h3 className="font-semibold mb-1">{title}</h3>
+                        <h3 className="font-semibold mb-1 text-left w-full">{title}</h3>
                         <p className="line-clamp-2" title={excerpt}>
                             {excerpt}
                         </p>
