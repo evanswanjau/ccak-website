5bcf3750de25f5f2b9c73f8b09915972f2751ac6
Evans Wanjau <evanswanjau@gmail.com>
2023-08-14 23:41:15 +0300

    update: provided skeleton loader for events and photoGallery

--- src/components/skeletonLoader.js
+++ src/components/skeletonLoader.js
@@ -1,14 +1,26 @@
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export const SkeletonLoader = () => {
+export const SkeletonLoader = ({ type }) => {
     return (
-        <div>
-            <Skeleton height={"150px"} />
-            <div className="my-1"></div>
-            <Skeleton width={"120px"} />
-            <div className="my-1"></div>
-            <Skeleton count={2} />
-        </div>
+        <>
+            {type === "news" && (
+                <div>
+                    <Skeleton height={"150px"} />
+                    <div className="my-1"></div>
+                    <Skeleton width={"120px"} />
+                    <div className="my-1"></div>
+                    <Skeleton count={2} />
+                </div>
+            )}
+
+            {type === "event" && (
+                <div>
+                    <Skeleton height={"220px"} />
+                    <div className="my-1"></div>
+                    <Skeleton width={"120px"} />
+                </div>
+            )}
+        </>
     );
 };
