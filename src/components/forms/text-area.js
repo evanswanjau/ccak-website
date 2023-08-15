530b08eb147d517ebc8a52a41ef7acb1a0173297
james-mumo <mumojames98@gmail.com>
2023-08-15 06:11:16 +0300

    update modal fix and integration

--- src/components/forms/text-area.js
+++ src/components/forms/text-area.js
@@ -1,30 +1,30 @@
 export const TextArea = ({
-    type,
-    name,
-    label,
-    required = false,
-    data,
-    updateData,
+  type,
+  name,
+  label,
+  required = false,
+  data,
+  updateData,
 }) => {
-    return (
-        <div>
-            <label
-                for={type}
-                class="block uppercase mb-2 text-[0.7em] font-bold text-gray-900 font-montserrat"
-            >
-                {name}
-                {required && <span className="text-red-600 text-sm">*</span>}
-            </label>
-            <textarea
-                id={type}
-                rows="3"
-                placeholder={label}
-                class="border font-manjari text-gray-900 rounded-lg focus:outline-none focus:ring-[#ED7423] focus:border-[#ED7423] block w-full p-2 pt-3"
-                value={data[name]}
-                onChange={(event) => {
-                    updateData({ ...data, [name]: event.target.value });
-                }}
-            ></textarea>
-        </div>
-    );
+  return (
+    <div>
+      <label
+        htmlFor={type}
+        className="block uppercase mb-2 text-[0.7em] font-bold text-gray-900 font-montserrat"
+      >
+        {name}
+        {required && <span className="text-red-600 text-sm">*</span>}
+      </label>
+      <textarea
+        id={type}
+        rows="3"
+        placeholder={label}
+        className="border font-manjari text-gray-900 rounded-lg focus:outline-none focus:ring-[#ED7423] focus:border-[#ED7423] block w-full p-2 pt-3"
+        value={data[name]}
+        onChange={(event) => {
+          updateData({ ...data, [name]: event.target.value });
+        }}
+      ></textarea>
+    </div>
+  );
 };
