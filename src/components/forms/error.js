e954994cae576da5265e0605c8f08e2e38607128
evanswanjau <evanswanjau@gmail.com>
2023-08-22 12:23:05 +0300

    fix: removed darkmode classes

--- src/components/forms/error.js
+++ src/components/forms/error.js
@@ -2,7 +2,7 @@ export const ErrorMessage = ({ error, setError }) => {
     return (
         <div
             id="alert-2"
-            className="flex p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+            className="flex p-4 mb-4 text-red-800 rounded-lg bg-red-50 "
             role="alert"
         >
             <svg
@@ -24,7 +24,7 @@ export const ErrorMessage = ({ error, setError }) => {
             </div>
             <button
                 type="button"
-                className="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+                className="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8"
                 data-dismiss-target="#alert-2"
                 aria-label="Close"
                 onClick={() => {
