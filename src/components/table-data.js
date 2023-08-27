cf0249aec3d8679963cf625e58451dbeed53615e
evanswanjau <evanswanjau@gmail.com>
2023-08-27 22:44:57 +0300

    fix: class= to className=

--- src/components/table-data.js
+++ src/components/table-data.js
@@ -1,16 +1,16 @@
 export const TableData = ({ titles, data }) => {
     return (
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="font-manjari w-full text-sm text-left text-gray-500">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="font-manjari w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr className="border-b">
-                        <th scope="col" class="px-3 py-3 border-r">
+                        <th scope="col" className="px-3 py-3 border-r">
                             #
                         </th>
 
                         {titles.map((title) => {
                             return (
-                                <th scope="col" class="px-3 py-3 border-r">
+                                <th scope="col" className="px-3 py-3 border-r">
                                     {title.name}
                                 </th>
                             );
@@ -20,11 +20,11 @@ export const TableData = ({ titles, data }) => {
                 <tbody>
                     {data.map((item, i) => {
                         return (
-                            <tr class="bg-white border-b border-gray-100">
-                                <td class="px-3 py-4 border-r">{i + 1}</td>
+                            <tr className="bg-white border-b border-gray-100">
+                                <td className="px-3 py-4 border-r">{i + 1}</td>
                                 {titles.map((title) => {
                                     return (
-                                        <td class="py-4 px-3 capitalize border-r border-gray-100">
+                                        <td className="py-4 px-3 capitalize border-r border-gray-100">
                                             {item[title.key]}
                                         </td>
                                     );
