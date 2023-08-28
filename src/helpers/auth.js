64c1a3a4eab62d0820aefeb1cfea75b5d0ccf84f
evanswanjau <evanswanjau@gmail.com>
2023-08-28 16:18:04 +0300

    Added member auth helper

---
+++ src/helpers/auth.js
@@ -0,0 +1,23 @@
+export const AuthMember = (jwt_decode, emailToken = null) => {
+    const token = emailToken ? emailToken : localStorage.getItem("token");
+
+    if (!token) {
+        window.location.replace("/membership/login");
+    } else {
+        const decodedToken = jwt_decode(token);
+
+        if (!decodedToken || isTokenExpired(decodedToken)) {
+            logout();
+        }
+    }
+};
+
+export const logout = () => {
+    localStorage.setItem("token", "");
+    window.location.replace("/login");
+};
+
+const isTokenExpired = (decodedToken) => {
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decodedToken.exp < currentTime;
+};
