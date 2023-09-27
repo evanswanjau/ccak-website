c2133a51f87ffdefdc7dbb5152c6bd30c203d5f5
evanswanjau <evanswanjau@gmail.com>
2023-09-27 20:51:42 +0300

    changed class= to className=

--- src/components/btnLoader.js
+++ src/components/btnLoader.js
@@ -1,10 +1,10 @@
 export const ButtonLoader = () => {
     return (
-        <div class="text-center">
+        <div className="text-center">
             <div role="status">
                 <svg
                     aria-hidden="true"
-                    class="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
+                    className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ export const ButtonLoader = () => {
                         fill="currentFill"
                     />
                 </svg>
-                <span class="sr-only">Loading...</span>
+                <span className="sr-only">Loading...</span>
             </div>
         </div>
     );
--- src/components/loader.js
+++ src/components/loader.js
@@ -1,10 +1,10 @@
 export const Loader = () => {
     return (
-        <div class="text-center">
+        <div className="text-center">
             <div role="status">
                 <svg
                     aria-hidden="true"
-                    class="inline w-8 h-8 mr-2 text-gray-200 animate-spin fill-green-600"
+                    className="inline w-8 h-8 mr-2 text-gray-200 animate-spin fill-green-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ export const Loader = () => {
                         fill="currentFill"
                     />
                 </svg>
-                <span class="sr-only">Loading...</span>
+                <span className="sr-only">Loading...</span>
             </div>
         </div>
     );
