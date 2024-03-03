8c576f7de69324b6af3526d80538c9a2165f1cfe
Evans Wanjau <evanswanjau@gmail.com>
2024-03-03 23:33:00 +0300

    Refactor Research component layout

--- src/components/research.js
+++ src/components/research.js
@@ -14,16 +14,16 @@ export const Research = ({
                     .toLowerCase()}`}
             >
                 <div className="bg-gray-200 group text-left p-5 rounded-lg transition duration-300 ease-in-out hover:bg-[#ED7423] flex flex-col h-full">
-                    <div className="flex w-full justify-between border-b border-gray-400 pb-3 group-hover:border-white transition duration-150 ease-in-out">
-                        <div>
-                            <h3 className="w-10/12 text-xl font-bold mb-1 group-hover:text-white transition duration-150 ease-in-out">
+                    <div className="flex w-full justify-between items-start border-b border-gray-400 pb-3 group-hover:border-white transition duration-150 ease-in-out">
+                        <div className="w-10/12">
+                            <h3 className="text-xl font-bold mb-1 group-hover:text-white transition duration-150 ease-in-out">
                                 {title}
                             </h3>
                             <p className="text-sm text-gray-400 group-hover:text-white transition duration-150 ease-in-out">
                                 {simpleDate(published)}
                             </p>
                         </div>
-                        <div className="w-2/12 flex justify-end items-center">
+                        <div className="w-2/12 flex justify-end">
                             <BsFileEarmarkPdf className="text-5xl group-hover:text-white transition duration-150 ease-in-out" />
                         </div>
                     </div>
